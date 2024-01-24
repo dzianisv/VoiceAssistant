6b742b349daeebaaab2075591dfb383e34b23bcc
Dennis <2119348+dzianisv@users.noreply.github.com>
2024-01-24 04:27:44 +0000

    Add ChatPromptTemplate

--- src/llm_langchains.py
+++ src/llm_langchains.py
@@ -2,10 +2,29 @@ from langchain_openai import ChatOpenAI
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferWindowMemory
 
+from langchain.chains import LLMChain
+from langchain.prompts import (
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate,
+    MessagesPlaceholder,
+    SystemMessagePromptTemplate,
+)
+
 
 class LLM(object):
     def __init__(self, api_key: str, memory_window=8):
-        memory = ConversationBufferWindowMemory(k=memory_window)
+        prompt = ChatPromptTemplate(
+            messages=[
+                SystemMessagePromptTemplate.from_template("""You are voice assistant Jarvis. \
+                    If you don't know the answer, just say that you don't know. \
+                    Use three sentences maximum and keep the answer concise"""
+                ),
+                MessagesPlaceholder(variable_name="chat_history"),
+                HumanMessagePromptTemplate.from_template("{question}"),
+            ]
+        )
+
+        memory = ConversationBufferWindowMemory(k=memory_window, memory_key="chat_history", return_messages=True)
 
         tools = [
             {
@@ -31,8 +50,9 @@ class LLM(object):
             }
         ]
         
-        llm = ChatOpenAI(openai_api_key=api_key, temperature=0.7, model="gpt-3.5-turbo").bind(tools=tools)
-        self.conversation = ConversationChain(llm=llm, memory=memory, verbose=False)
+        llm = ChatOpenAI(openai_api_key=api_key, temperature=0.7, model="gpt-3.5-turbo")
+        self.conversation = LLMChain(llm=llm, prompt=prompt, verbose=True, memory=memory)
+        # self.conversation = ConversationChain(llm=llm, memory=memory, verbose=False)
 
     def ask(self, prompt):
-        return self.conversation.predict(input=prompt)
+        return self.conversation(prompt)['text']
