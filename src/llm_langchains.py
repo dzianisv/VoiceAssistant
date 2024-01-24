4e89b701c3dc35ca90faccbe813399d1077b6b12
Dennis <2119348+dzianisv@users.noreply.github.com>
2024-01-24 04:47:19 +0000

    Add ChatPromptTemplate

--- src/llm_langchains.py
+++ src/llm_langchains.py
@@ -1,5 +1,4 @@
 from langchain_openai import ChatOpenAI
-from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferWindowMemory
 
 from langchain.chains import LLMChain
@@ -10,6 +9,7 @@ from langchain.prompts import (
     SystemMessagePromptTemplate,
 )
 
+from langchain_community.utilities.openweathermap import OpenWeatherMapAPIWrapper
 
 class LLM(object):
     def __init__(self, api_key: str, memory_window=8):
@@ -26,33 +26,15 @@ class LLM(object):
 
         memory = ConversationBufferWindowMemory(k=memory_window, memory_key="chat_history", return_messages=True)
 
-        tools = [
-            {
-                "type": "function",
-                "function": {
-                    "name": "get_current_weather",
-                    "description": "Get the current weather in a given location",
-                    "parameters": {
-                        "type": "object",
-                        "properties": {
-                            "location": {
-                                "type": "string",
-                                "description": "The city and state, e.g. San Francisco, CA",
-                            },
-                            "unit": {
-                                "type": "string",
-                                "enum": ["celsius", "fahrenheit"],
-                            },
-                        },
-                        "required": ["location"],
-                    },
-                },
-            }
-        ]
-        
+        weather_tool = Tool(
+            name="weather", 
+            func=OpenWeatherMapAPIWrapper(os.getenv("OPENWEATHERMAP_API_KEY")), 
+            description="Get weather data"
+        )
+
         llm = ChatOpenAI(openai_api_key=api_key, temperature=0.7, model="gpt-3.5-turbo")
-        self.conversation = LLMChain(llm=llm, prompt=prompt, verbose=True, memory=memory)
-        # self.conversation = ConversationChain(llm=llm, memory=memory, verbose=False)
+        agent = Agent(tools=[weather_tool], llm=llm, prompt=prompt, verbose=True, memory=memory)
+        # self.conversation = LLMChain(llm=llm, )
 
     def ask(self, prompt):
         return self.conversation(prompt)['text']
