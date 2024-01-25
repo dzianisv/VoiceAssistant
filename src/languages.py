1b140297d2b246e65526f022c1630edc132a68b2
Dennis <2119348+dzianisv@users.noreply.github.com>
2024-01-25 13:21:29 +0000

    Add english language pack

---
+++ src/languages.py
@@ -0,0 +1,15 @@
+class RuLanguagePack:
+    llm_query = "Подождите, сейчас узнаю..."
+    greeting_message = "Как я могу помочь тебе?"
+    iam_on = "Я включился!"
+    
+    google_stt_lang = 'ru-RU'
+    google_tts_lang = 'ru'
+    
+class EnLanguagePack:
+    llm_query = "Wait a little bit..."
+    greeting_message = "How I can help you?"
+    iam_on = "I am available!"
+    
+    google_stt_lang = 'en-US'
+    google_tts_lang = 'en'
