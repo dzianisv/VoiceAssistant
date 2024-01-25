7816a6d68433e631eafca5acabdf72b14fd5596f
Dennis <2119348+dzianisv@users.noreply.github.com>
2024-01-25 19:17:50 +0000

    Add wakeword actions

--- src/main.py
+++ src/main.py
@@ -14,14 +14,17 @@ logger.setLevel(logging.DEBUG)
 logger.addHandler(logging.StreamHandler(sys.stderr))
 
 from hal import detect
+
 hal = detect()
-hal.start_blink((1,2))
+hal.start_blink((1, 2))
 
 from languages import languages
+
 lang_pack = languages[os.getenv("LANGUAGE", "ru")]
 
 logger.info("loading llm...")
 from llm_langchains import LLM
+
 logger.info("loading wake word engine...")
 import wakeword_pvporcupine as wakeword
 
@@ -32,22 +35,27 @@ elif llm_type == "openai":
     llm = LLM(api_key=os.getenv("OPENAI_KEY"))
 
 from stt_speechrecognition import STT
+
 stt = STT(language=lang_pack.google_stt_lang)
 
-tts_type = 'google'
-if tts_type == 'rhvoice':
+tts_type = "google"
+if tts_type == "rhvoice":
     from tts_rhvoice import TTS
+
     tts = TTS()
-elif tts_type == 'google':
+elif tts_type == "google":
     from tts_gtts import TTS
+
     tts = TTS()
 
+
 def speak(text, block=True) -> bool:
-    hal.start_blink((0.5,2))
+    hal.start_blink((0.5, 2))
     try:
         return tts.speak(text, block)
     finally:
         hal.stop_blink()
+
 
 def listen() -> str:
     hal.led_on()
@@ -56,11 +64,13 @@ def listen() -> str:
     finally:
         hal.led_off()
 
+
 def wait_for_activation_keyword():
-    hal.start_blink((0.5,10))
+    hal.start_blink((0.5, 10))
     keyword = wakeword.wait()
-    logger.debug("recognezed an activation keyword \"%s\"", keyword)
+    logger.debug('recognezed an activation keyword "%s"', keyword)
     communicate()
+
 
 def communicate():
     text = lang_pack.greeting_message
@@ -68,28 +78,32 @@ def communicate():
     while speak(text):
         logger.info("Listening...")
         question = listen()
-        
-        if question in set(['забудь', 'проехали', 'отмена', 'stop', 'cancel', 'never mind']):
+
+        if question in languages.stop_words:
+            speak(lang_pack.ok)
             break
-        
+
         if question:
             speak(lang_pack.llm_query, block=False)
-            hal.start_blink((0.5,1))
+            hal.start_blink((0.5, 1))
             text = llm.ask(question)
             logger.info("LLM: %s", text)
 
             queues = actions.ActionsQueue()
-            
+
             if actions.run(text, queues):
-                wakeword.wait()
+                action = wakeword.wait()
                 queues.down.put(actions.Commands.STOP.value)
                 text = lang_pack.greeting_message
-                continue
+
+                if action == wakeword.KeywordSpottingActions.STOP:
+                    break
+                elif action == wakeword.KeywordSpottingActions.HEY:
+                    continue
         else:
             break
 
     wait_for_activation_keyword()
-
 
 
 if __name__ == "__main__":
--- src/wakeword_pvporcupine.py
+++ src/wakeword_pvporcupine.py
@@ -1,4 +1,19 @@
 import os
+import logging
+import sys
+import enum
+
+
+class KeywordSpottingActions(enum.Enum):
+    HEY = "hey"
+    STOP = "stop"
+
+
+# initialize logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stderr))
+
 
 import pvporcupine
 from pvrecorder import PvRecorder
@@ -7,40 +22,49 @@ from pvrecorder import PvRecorder
 {'pico clock', 'hey google', 'grapefruit', 'picovoice', 'blueberry', 'ok google', 'jarvis', 'hey siri', 'bumblebee', 'alexa', 'hey barista', 'grasshopper', 'porcupine', 'americano', 'computer', 'terminator'}
 >>> """
 
-keywords_en = ["bumblebee", "blueberry", "jarvis", "please stop"],
-porcupine_en = pvporcupine.create(
-    access_key=os.getenv("PVPORCUPINE_ACCESS_KEY"),
-    keywords= keywords_en,
-    keyword_paths=[
-         os.path.join(os.path.dirname(__file__), "please-stop_en_arm.ppn"),
-    ]
-)
-
-# ru_keywords = ["товарищ", "отмена"]
-# porcupine_ru = pvporcupine.create(
+# TODO: it can't mix keywords and keyword_paths
+# keywords_en = ["bumblebee", "blueberry", "jarvis", "ok google", "hey siri", "alexa", "please stop"],
+# porcupine_en = pvporcupine.create(
 #     access_key=os.getenv("PVPORCUPINE_ACCESS_KEY"),
+#     keywords= keywords_en[:-1],
 #     keyword_paths=[
-#         os.path.join(os.path.dirname(__file__), "Товарищ_ru_arm.ppn"),
-#         os.path.join(os.path.dirname(__file__), "отмена_ru_arm.ppn"),
-#     ],
-#     model_path=os.path.join(os.path.dirname(__file__), "porcupine_params_ru.pv"),
+#          os.path.join(os.path.dirname(__file__), "please-stop_en_arm.ppn"),
+#     ]
 # )
 
+actions_ru = [KeywordSpottingActions.HEY, KeywordSpottingActions.STOP]
+keywords_ru = ["товарищ", "отмена"]
+porcupine_ru = pvporcupine.create(
+    access_key=os.getenv("PVPORCUPINE_ACCESS_KEY"),
+    keyword_paths=[
+        os.path.join(os.path.dirname(__file__), "Товарищ_ru_arm.ppn"),
+        os.path.join(os.path.dirname(__file__), "отмена_ru_arm.ppn"),
+    ],
+    model_path=os.path.join(os.path.dirname(__file__), "porcupine_params_ru.pv"),
+)
+
+
 class Model:
-    recognizer = porcupine_en
-    keywords = keywords_en
+    recognizer = porcupine_ru
+    keywords = keywords_ru
+    actions = actions_ru
+
 
 model = Model
 
+
 def wait():
     try:
-        recorder = PvRecorder(device_index=-1, frame_length=porcupine.frame_length)
+        recorder = PvRecorder(
+            device_index=-1, frame_length=model.recognizer.frame_length
+        )
         recorder.start()
         while True:
             audio_frame = recorder.read()
             keyword_index = model.recognizer.process(audio_frame)
             if keyword_index > -1:
-                return mode.keywords[keyword_index]
+                logger.info('detected keyword "%s"', model.keywords[keyword_index])
+                return model.actions[keyword_index]
     finally:
         recorder.stop()
         recorder.delete()
