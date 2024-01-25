02904c6c5a47beaa64ed07a57b65d9ee35d3b977
Dennis <2119348+dzianisv@users.noreply.github.com>
2024-01-25 19:00:03 +0000

    Add please-stop

--- src/wakeword_pvporcupine.py
+++ src/wakeword_pvporcupine.py
@@ -7,13 +7,30 @@ from pvrecorder import PvRecorder
 {'pico clock', 'hey google', 'grapefruit', 'picovoice', 'blueberry', 'ok google', 'jarvis', 'hey siri', 'bumblebee', 'alexa', 'hey barista', 'grasshopper', 'porcupine', 'americano', 'computer', 'terminator'}
 >>> """
 
-
-porcupine = pvporcupine.create(
-  access_key=os.getenv("PVPORCUPINE_ACCESS_KEY"),
-  keywords=["bumblebee", "blueberry", "jarvis"]
-#   keyword_paths=[os.path.join(os.path.dirname(__file__), 'Товарищ_ru.ppn')],
-#   model_path=os.path.join(os.path.dirname(__file__), 'porcupine_params_ru.pv'),
+keywords_en = ["bumblebee", "blueberry", "jarvis", "please stop"],
+porcupine_en = pvporcupine.create(
+    access_key=os.getenv("PVPORCUPINE_ACCESS_KEY"),
+    keywords= keywords_en,
+    keyword_paths=[
+         os.path.join(os.path.dirname(__file__), "please-stop_en_arm.ppn"),
+    ]
 )
+
+# ru_keywords = ["товарищ", "отмена"]
+# porcupine_ru = pvporcupine.create(
+#     access_key=os.getenv("PVPORCUPINE_ACCESS_KEY"),
+#     keyword_paths=[
+#         os.path.join(os.path.dirname(__file__), "Товарищ_ru_arm.ppn"),
+#         os.path.join(os.path.dirname(__file__), "отмена_ru_arm.ppn"),
+#     ],
+#     model_path=os.path.join(os.path.dirname(__file__), "porcupine_params_ru.pv"),
+# )
+
+class Model:
+    recognizer = porcupine_en
+    keywords = keywords_en
+
+model = Model
 
 def wait():
     try:
@@ -21,10 +38,9 @@ def wait():
         recorder.start()
         while True:
             audio_frame = recorder.read()
-            keyword_index = porcupine.process(audio_frame)
-            if keyword_index > 0:
-                return True
+            keyword_index = model.recognizer.process(audio_frame)
+            if keyword_index > -1:
+                return mode.keywords[keyword_index]
     finally:
         recorder.stop()
         recorder.delete()
-        
