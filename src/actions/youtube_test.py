7273af8ccdc1f8368dea021381e89be713215ecb
Dennis <2119348+dzianisv@users.noreply.github.com>
2024-01-25 08:42:56 +0000

    Support playing the multiple youtube streams

--- src/actions/youtube_test.py
+++ src/actions/youtube_test.py
@@ -4,12 +4,18 @@ import unittest
 import os
 
 from youtube import PlayYoutube
+from utils import ActionsQueue
+import time
+
 
 class TestLLM(unittest.TestCase):
     def test_playing(self):
         message = "answer The link to 'Four Seasons' by Antonio Vivaldi on YouTube is [here](https://www.youtube.com/watch?v=GRxofEmo3HA&pp=ygUcQW50b25pbyBWaXZhbGRpIEZvdXIgU2Vhc29ucw%3D%3D)."
         a = PlayYoutube()
-        assert(a.run(message))
+        q = ActionsQueue()
+        assert(a.run(message, q))
+        time.sleep(30)
+        q.down.put("STOP")
 
 if __name__ == '__main__':
     unittest.main()
