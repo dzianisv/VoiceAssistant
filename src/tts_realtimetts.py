d86a416442b7a87fc5e6daf7ad78d522b634c33e
Den <2119348+dzianisv@users.noreply.github.com>
2024-08-11 16:46:22 +0300

    Cache realtimetts

---
+++ src/tts_realtimetts.py
@@ -0,0 +1,59 @@
+from io import BytesIO
+import os
+from languages import detect_language
+import logging
+import sys
+from pydispatch import dispatcher
+from RealtimeTTS import TextToAudioStream, OpenAIEngine
+import httpx
+import os
+import hashlib
+import pygame
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stderr))
+
+
+def md5hash(data: str) -> str:
+    return hashlib.md5(data.encode()).hexdigest()
+
+class TTS:
+    def __init__(self):
+        self.workdir = os.path.join(os.getcwd(), 'gtts')
+        
+        if not os.path.exists(self.workdir):
+            os.makedirs(self.workdir)
+            
+        dispatcher.connect(self.stop, signal='stop', sender=dispatcher.Any)
+
+         # alloy, echo, fable, onyx, nova, and shimmer
+        proxy_url = os.environ.get("OPENAI_PROXY")
+        http_client=httpx.Client(proxy=proxy_url)
+        self.engine = OpenAIEngine(model='tts-1', voice='echo', openai_args={"http_client": http_client})
+        self.stream = None
+
+
+    def play(self, filename: str):
+        pygame.mixer.music.load(filename)
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            pygame.time.Clock().tick(10)
+
+    def stop(self):
+        if self.stream:
+             self.stream.stop()
+        else:
+            pygame.mixer.music.stop()
+
+
+    def speak(self, text):
+        cache_key = md5hash(text)
+        cache_file = os.path.join(self.workdir, f"{cache_key}.wav")
+        if os.path.exists(cache_file):
+            self.play(cache_file)
+        else:
+            # https://platform.openai.com/docs/guides/text-to-speech/quickstart
+            self.stream = TextToAudioStream(self.engine)
+            self.stream.feed(text)
+            self.stream.play_async(coutput_wavfile=cache_file)
